dc7a3049af577662af70a1091b660e64f50b8caa
UG3800 <47650356+UG3800@users.noreply.github.com>
2023-01-08 17:16:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-#=================================================================#
-#   System Required: CentOS7 X86_64                               #
-#   Description: FFmpeg Stream Media Server                       #
-#   Author: LALA                                    #
-#   Website: https://www.lala.im                                  #
-#=================================================================#
 
 # 颜色选择
 red='\033[0;31m'
